65b99980605fd568dae3789f5c4662b3743d9801
kingwilldabeast <willmur@live.com>
2024-06-10 16:24:25 -0500

    completed MVP

---
+++ reactville-project/src/WeatherForecast.jsx
@@ -0,0 +1,15 @@
+
+export default function WeatherForecast (props) {
+
+    return (
+        <>
+        <div className="weather">
+        <h2>{props.forecast.day}</h2>
+        <img src={props.forecast.img} alt={props.forecast.imgAlt} />
+        <p><span>conditions: </span>{props.forecast.conditions}</p>
+        <p><span>time: </span>{props.forecast.time}</p>
+        </div>
+        </>
+    )
+
+}
